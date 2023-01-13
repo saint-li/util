61f08bd151231173b798bf8b1b80d668d4551e3d
李德龙 <saint.li@qq.com>
2023-01-13 14:59:48 +0800

    网络请求增加Debug模式设置无代理

--- utillib/src/main/java/com/saint/util/http/OkUtil.java
+++ utillib/src/main/java/com/saint/util/http/OkUtil.java
@@ -13,6 +13,7 @@ import com.saint.util.util.GsonUtil;
 import okhttp3.OkHttpClient;
 
 import java.io.File;
+import java.net.Proxy;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
@@ -25,6 +26,7 @@ public class OkUtil {
     private static long connectTimeOut = 30;
     private static long writeTimeOut = 20;
     private static long readTimeOut = 20;
+//    private static boolean proxyModel = true;
     private final String TAG = "OK_UTIL";
 
     private OkGo mOkGo;
@@ -48,10 +50,9 @@ public class OkUtil {
     }
 
     /**
-     *
      * @param connectSecond 网络连接超时时间 单位：秒
-     * @param writeSecond 写数据超时时间 单位：秒
-     * @param readSecond 读取超时时间 单位：秒
+     * @param writeSecond   写数据超时时间 单位：秒
+     * @param readSecond    读取超时时间 单位：秒
      * @return
      */
     public static OkUtil instance(int connectSecond, int writeSecond, int readSecond) {
@@ -68,6 +69,27 @@ public class OkUtil {
         return okUtil;
     }
 
+//    /**
+//     * @param connectSecond 网络连接超时时间 单位：秒
+//     * @param writeSecond   写数据超时时间 单位：秒
+//     * @param readSecond    读取超时时间 单位：秒
+//     * @param proxyModel    代理模式 Debug模式可设置false
+//     * @return
+//     */
+//    public static OkUtil instance(int connectSecond, int writeSecond, int readSecond, boolean proxyModel) {
+//        if (okUtil == null) {
+//            synchronized (OkUtil.class) {
+//                if (okUtil == null) {
+//                    connectTimeOut = connectSecond;
+//                    writeTimeOut = writeSecond;
+//                    readTimeOut = readSecond;
+//                    okUtil = new OkUtil();
+//                }
+//            }
+//        }
+//        return okUtil;
+//    }
+
     public OkGo getOkGo() {
         return mOkGo;
     }
@@ -83,6 +105,10 @@ public class OkUtil {
         mBuilder.connectTimeout(connectTimeOut, TimeUnit.SECONDS);
         mBuilder.readTimeout(readTimeOut, TimeUnit.SECONDS);
         mBuilder.writeTimeout(writeTimeOut, TimeUnit.SECONDS);
+        //设置无代理模式
+        if (!UtilConfig.isDebug()) {
+            mBuilder.proxy(Proxy.NO_PROXY);
+        }
 
         //配置Https
         //方法一：信任所有证书,不安全有风险
@@ -105,10 +131,7 @@ public class OkUtil {
 //        HttpParams params = new HttpParams();
 //        params.put("key", "value");
 
-        mOkGo = OkGo.getInstance().init(UtilConfig.getApp())
-                .setOkHttpClient(mBuilder.build())
-                .setCacheMode(CacheMode.NO_CACHE)
-                .setRetryCount(0)
+        mOkGo = OkGo.getInstance().init(UtilConfig.getApp()).setOkHttpClient(mBuilder.build()).setCacheMode(CacheMode.NO_CACHE).setRetryCount(0)
 //                .addCommonHeaders(headers)
 //                .addCommonParams(params)
         ;
@@ -116,59 +139,35 @@ public class OkUtil {
 
 
     public void postData(String api, HttpParams params, Object tag, StringCallback callback) {
-        OkGo.<String>post(api)
-                .params(params)
-                .tag(tag)
-                .execute(callback);
+        OkGo.<String>post(api).params(params).tag(tag).execute(callback);
     }
 
     public void getData(String api, HttpParams params, Object tag, StringCallback callback) {
-        OkGo.<String>get(api)
-                .params(params)
-                .tag(tag)
-                .execute(callback);
+        OkGo.<String>get(api).params(params).tag(tag).execute(callback);
     }
 
     public void putData(String api, HttpParams params, Object tag, StringCallback callback) {
-        OkGo.<String>put(api)
-                .params(params)
-                .tag(tag)
-                .execute(callback);
+        OkGo.<String>put(api).params(params).tag(tag).execute(callback);
     }
 
     public void deleteData(String api, HttpParams params, Object tag, StringCallback callback) {
-        OkGo.<String>delete(api)
-                .tag(tag)
-                .isSpliceUrl(true)
-                .params(params)
-                .execute(callback);
+        OkGo.<String>delete(api).tag(tag).isSpliceUrl(true).params(params).execute(callback);
     }
 
     public void download(Object tag, String url, FileCallback callback) {
-        OkGo.<File>post(url)
-                .tag(tag)
-                .execute(callback);
+        OkGo.<File>post(url).tag(tag).execute(callback);
     }
 
     public void postJson(Object tag, String url, Object postObj, StringCallback callback) {
-        OkGo.<String>post(url)
-                .upJson(GsonUtil.instance().beanToJson(postObj))
-                .tag(tag)
-                .execute(callback);
+        OkGo.<String>post(url).upJson(GsonUtil.instance().beanToJson(postObj)).tag(tag).execute(callback);
     }
 
     public void putJson(Object tag, String url, Object postObj, StringCallback callback) {
-        OkGo.<String>put(url)
-                .upJson(GsonUtil.instance().beanToJson(postObj))
-                .tag(tag)
-                .execute(callback);
+        OkGo.<String>put(url).upJson(GsonUtil.instance().beanToJson(postObj)).tag(tag).execute(callback);
     }
 
     public void deleteJson(Object tag, String url, Object postObj, StringCallback callback) {
-        OkGo.<String>delete(url)
-                .upJson(GsonUtil.instance().beanToJson(postObj))
-                .tag(tag)
-                .execute(callback);
+        OkGo.<String>delete(url).upJson(GsonUtil.instance().beanToJson(postObj)).tag(tag).execute(callback);
     }
 
     public void cancelTag(Object obj) {
